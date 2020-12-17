b6f6389f07a20138919519874e011bb9622987a4
Aaron Esau <git@aaronesau.com>
2020-12-16 18:40:17 -0800

    move stuff to header file

--- malloc.c
+++ malloc.c
@@ -1,18 +1,6 @@
 #include "stdlib.h"
 #include "string.h"
-
-#define _MAX_ADDRESS 0xFFFF
-
-typedef struct chunk_meta {
-    size_t size;
-    void *freeptr;
-} chunk_meta;
-
-void *free_ptr_head = NULL;
-void *bottom_ptr = _MAX_ADDRESS;
-
-#define IS_CHUNK_FREE(chunk) (!(chunk)->freeptr)
-#define GET_CHUNK_META(ptr) (chunk_meta *) ((ptr) - sizeof(chunk_meta))
+#include "malloc.h"
 
 
 void *malloc(size_t size) {
---
+++ malloc.h
@@ -0,0 +1,13 @@
+typedef struct chunk_meta {
+    size_t size;
+    void *freeptr;
+} chunk_meta;
+
+#define _MAX_ADDRESS 0xFFFF
+
+void *free_ptr_head = NULL;
+void *bottom_ptr = _MAX_ADDRESS;
+
+#define IS_CHUNK_FREE(chunk) (!(chunk)->freeptr)
+#define GET_CHUNK_META(ptr) (chunk_meta *) ((ptr) - sizeof(chunk_meta))
+
